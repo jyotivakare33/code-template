4b0e8e76e7180f0279f090d68b198cb0bc6e2a8b
jyoti vakare <jyotivakare33@gmail.com>
2021-02-26 16:41:28 +0530

    translator css

--- translator.js
+++ translator.js
@@ -4,12 +4,10 @@ const transalte = document.querySelector('.btn-translate');
 const inputValue = document.querySelector('.input');
 
 transalte.addEventListener('click', () => {
-   // if (inputValue.value !== '') {
-        fetch(`https://api.mymemory.translated.net/get?q=`+inputValue.value+`&langpair=`+dropdown.value+'|'+dropdownOutput.value)
-                .then((response) => response.json())
-                .then((json) => {
-                    console.log(json.responseData.translatedText);
-                    document.querySelector('.result').innerHTML = json.responseData.translatedText;
-                });
-   // }
+    fetch(`https://api.mymemory.translated.net/get?q=`+inputValue.value+`&langpair=`+dropdown.value+'|'+dropdownOutput.value)
+        .then((response) => response.json())
+        .then((json) => {
+            document.querySelector('.result').removeAttribute('hidden');
+            document.querySelector('.result').innerHTML = json.responseData.translatedText;
+    });
 });
